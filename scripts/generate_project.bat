2e1c86ca2709feb03e1aa53a8b69fc0c7760331b
Milos Tosic <milostosic77@gmail.com>
2023-09-08 12:34:28 +0200

    Update project generating batch

--- scripts/generate_project.bat
+++ scripts/generate_project.bat
@@ -1,4 +1,4 @@
 @echo off
-cd ..\genie
-..\build\tools\bin\windows\genie.exe vs2022
-cd ..\scripts
+cd ..\src\app_template\genie
+..\..\..\build\tools\bin\windows\genie.exe vs2022
+cd ..\..\..\scripts
