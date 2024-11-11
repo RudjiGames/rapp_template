f9b5c3d650919c1209f4f0c866fdf0abb0896f4e
Milos Tosic <milostosic77@gmail.com>
2024-11-11 20:06:50 +0100

    Fixes for xcode

--- scripts/osx_generate_project.sh
+++ scripts/osx_generate_project.sh
@@ -13,6 +13,10 @@ if [[ "$target" == "cheerp" ]]; then
 action="--gcc=cheerp gmake"
 fi
 
+if [[ "$target" == "xcode" ]]; then 
+action="xcode15"
+fi
+
 cd ../src/app_template/genie
 ../../../build/tools/bin/darwin/genie $action
 cd ../../../scripts
