8a5e0ce8b3a7e35e1bd7795df7aec37946b763d0
milos.tosic <milos.tosic@buildarocketboy.com>
2024-11-24 00:35:08 +0100

    Placeholder build fixes, ps4/5

--- scripts/win_generate_project.bat
+++ scripts/win_generate_project.bat
@@ -2,6 +2,8 @@
 set ACTION=vs2022
 IF "%1"=="emscripten" set ACTION=--with-miniaudio-only --gcc=wasm2js gmake
 IF "%1"=="cheerp"     set ACTION=--with-miniaudio-only --gcc=cheerp gmake
+IF "%1"=="orbis"      set ACTION=--gcc=orbis gmake
+IF "%1"=="prospero"   set ACTION=--gcc=prospero gmake
 cd ..\src\app_template\genie
 ..\..\..\build\tools\bin\windows\genie.exe %ACTION%
 cd ..\..\..\scripts
